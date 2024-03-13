48c1a4a59ee8dbf4af6c83f1687d1570b0cc3b3f
Ben Wallace <bencwallace@gmail.com>
2024-03-13 18:39:13 +0000

    move flatten and permute to views

--- src/ops.cpp
+++ src/ops.cpp
@@ -26,33 +26,6 @@ Tensor dot(const Tensor &left, const Tensor &right) {
   return result;
 }
 
-Tensor flatten(const Tensor &tensor) {
-  return Tensor(Array({tensor.size()}), Array({1}), tensor);
-}
-
-Tensor permute(const Tensor &tensor, std::initializer_list<size_t> axes) {
-  if (axes.size() != tensor.ndims()) {
-    std::stringstream ss;
-    ss << "Expected axis list of length " << tensor.ndims() << ", got "
-       << axes.size();
-    throw std::invalid_argument(ss.str());
-  }
-
-  const Array &shape = tensor.shape();
-  const Array &strides = tensor.strides();
-
-  Array result_shape(tensor.ndims());
-  Array result_strides(tensor.ndims());
-  size_t i = 0;
-  for (size_t axis : axes) {
-    result_shape[i] = shape[axis];
-    result_strides[i] = strides[axis];
-    ++i;
-  }
-
-  return Tensor(result_shape, result_strides, tensor);
-}
-
 double norm2(const Tensor &tensor) {
   Tensor flat = flatten(tensor);
   return static_cast<double>(dot(flat, flat));
