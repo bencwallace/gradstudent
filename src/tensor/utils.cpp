5a8fe9f202dea0e3ef3c6c09b08245dc0bd73b3f
Ben Wallace <bencwallace@gmail.com>
2024-03-19 15:41:18 -0400

    fix issue in copy-on-write mechanism

--- src/tensor/utils.cpp
+++ src/tensor/utils.cpp
@@ -7,7 +7,10 @@ void Tensor::ensureWritable() {
   // should be called prior to any write operation
   if (ro_) {
     double *temp = new double[size_];
-    std::memcpy(temp, data_.get(), size_ * sizeof(double));
+    size_t i = 0;
+    for (auto mIdx : multiIndexRange()) {
+      temp[i++] = data_[toIndex(mIdx)];
+    }
     data_.reset(temp);
     ro_ = false;
   }
