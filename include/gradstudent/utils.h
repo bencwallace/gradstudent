cf4cb468ec66282e0a66d03501bc931ce39c1283
Ben Wallace <bencwallace@gmail.com>
2024-04-22 12:19:53 +0200

    document utils header

--- include/gradstudent/utils.h
+++ include/gradstudent/utils.h
@@ -1,3 +1,15 @@
+/**
+ * @file utils.h
+ * @author Ben Wallace (me@bcwallace.com)
+ * @brief Utility functions
+ * @version 0.1
+ * @date 2024-04-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#pragma once
+
 #include <array>
 #include <cstring>
 #include <istream>
@@ -7,6 +19,7 @@
 
 namespace gs {
 
+// @cond
 template <typename T>
 Tensor parse_numpy_data(const array_t &shape, std::istream &file) {
   Tensor result(shape);
@@ -19,11 +32,43 @@ Tensor parse_numpy_data(const array_t &shape, std::istream &file) {
   }
   return result;
 }
+// @endcond
 
+/**
+ * @brief Reads a file in the NPY format
+ *
+ * Currently, the following restrictions apply:
+ *
+ *   * Format version must be 1.0.
+ *   * `fortran_order` must be false (only C order supported).
+ *   * Array data may not contain Python objects.
+ *   * Array dtype must be `<f8` (little-endian 64-bit floating point).
+ *
+ * For more information: https://numpy.org/doc/stable/reference/generated/numpy.lib.format.html
+ *
+ * @param filename File to parse
+ * @return Tensor The tensor parsed out of the file
+ */
 Tensor read_numpy(const std::string &filename);
 
+/**
+ * @brief Reads a PGM image into a tensor
+ *
+ * For more information: https://netpbm.sourceforge.net/doc/pgm.html
+ *
+ * @param filename The image to read
+ * @return Tensor The corresponding tensor
+ */
 Tensor read_image(const std::string &filename);
 
+/**
+ * @brief Writes a tensor to an image in PGM format
+ *
+ * For more information: https://netpbm.sourceforge.net/doc/pgm.html
+ *
+ * @param filename The output path
+ * @param image The tensor to write
+ */
 void write_image(const std::string &filename, const Tensor &image);
 
 } // namespace gs
